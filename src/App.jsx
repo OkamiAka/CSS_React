ac3563d77eddcc7c12e30182a143649c3f8d91b4
OkamiAka <nils.caqueux@gmail.com>
2023-04-03 15:46:04 +0200

    commit all

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+const Card = styled.div`
+  background-color: red;
+`;
+
+function App() {
+  return (
+    <>
+      <Card>test</Card>
+      <div></div>
+    </>
+  );
+}
+
+export default App;
